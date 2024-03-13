6106dc849a3534010f3c1b8fb451dafe143d3cfe
mbenkhouya <mbenkhouya@fr-d7d40c2hyg.home>
2024-03-13 06:21:11 +0100

    Initial commit from Create Next App

---
+++ app/page.tsx
@@ -0,0 +1,116 @@
+export default function Home() {
+  return (
+    <main style={{"padding": "1rem 1.5rem"}}>
+      <div style={{"direction": "rtl"}}>
+        <h1><center>مرحبًا بكم في Lghorba</center></h1>
+        
+        
+        <p>مرحبًا بكم في Lghorba.com، وجهتكم على الإنترنت لاكتشاف مقالات مثيرة، وقصص ملهمة، ونصائح عملية.</p>
+        
+        <section>
+          <h3>حول الغُربة</h3>
+          
+          <p>Lghorba هو أكثر من مجرد مدونة. إنها مجتمع متحمس من الأشخاص ذوي الاهتمامات المشتركة، مكان يمكنكم فيه التواصل مع الآخرين ومشاركة تجاربكم. سواء كنتم مهتمين بالسفر، أو الطهي، أو الثقافة، أو التكنولوجيا، أو أي موضوع آخر، فإنكم بالتأكيد ستجدون شيئًا يهمكم هنا في Lghorba.</p>
+          
+        </section>
+          <section>
+            <h3>ما ستجدونه على مدونتنا</h3>
+            
+            <p>على مدونتنا، ستجدون مجموعة متنوعة من المحتويات، بما في ذلك:</p>
+          <ul>
+            <li>قصص سفر ملهمة ستنقلكم إلى وجهات بعيدة وستثير رغبتكم في استكشاف العالم.</li>
+            <li>وصفات شهية وسهلة التحضير لتروي غمرة جوعكم وتثير حواسكم.</li>
+            <li>مقالات ثقافية تسمح لكم باكتشاف تقاليد، وعادات، وتعابير فنية جديدة.</li>
+            <li>نصائح عملية حول التكنولوجيا، والعناية بالصحة، وزيادة الإنتاجية، وغيرها من المواضيع.</li>
+          </ul>
+          </section>
+        
+        <section>
+          <h3>انضموا إلى مجتمعنا</h3>
+          
+          <p>ندعوكم للانضمام إلى مجتمعنا عبر الاشتراك في النشرة الإخبارية لدينا ومتابعتنا على وسائل التواصل الاجتماعي. هذه هي أفضل طريقة للبقاء على اطلاع بآخر التحديثات، والمقالات، والفعاليات التي تقدمها Lghorba.</p>
+        </section>
+        
+        <section>
+          <h3>تواصلوا معنا</h3>
+          
+          <p>
+          إذا كان لديكم أي أسئلة، أو اقتراحات، أو إذا كنتم ترغبون في المساهمة ككتّاب ضيوف، فلا تترددوا في الاتصال بنا. نحن دائمًا سعداء بالتواصل مع قرائنا والتعاون مع المتحمسين مثلكم!
+          </p>
+        </section>
+          <h1><center>استكشفوا، واكتشفوا، وشاركوا مع Lghorba.com!</center></h1>
+      </div>
+      <hr />
+      <div>
+        <h1><center>Bienvenue sur Lghorba.com</center></h1>
+        
+        <p>Bienvenue sur Lghorba.com, votre destination en ligne pour découvrir des articles captivants, des histoires inspirantes et des conseils pratiques.</p>
+        
+        <section>
+          <h3>À propos de Lghorba</h3>
+          
+          <p>Lghorba est bien plus qu'un simple blog. C'est une communauté passionnée de personnes partageant les mêmes intérêts, un lieu où vous pouvez vous connecter avec d'autres individus et partager vos expériences. Que vous soyez passionné par les voyages, la cuisine, la culture, la technologie ou tout autre sujet, vous trouverez certainement quelque chose qui vous intéresse ici sur Lghorba.</p>
+          
+        </section>
+          <section>
+            <h3>Ce que vous trouverez sur notre blog</h3>
+            
+            <p>Sur notre blog, vous découvrirez une variété de contenus, notamment :</p>
+          <ul>
+            <li>Des récits de voyage inspirants qui vous transporteront dans des destinations exotiques et vous donneront envie d'explorer le monde.</li>
+            <li>Des recettes délicieuses et faciles à réaliser pour satisfaire votre appétit et éveiller vos papilles.</li>
+            <li>Des articles culturels qui vous permettront de découvrir de nouvelles traditions, coutumes et expressions artistiques.</li>
+            <li>Des conseils pratiques sur la technologie, le bien-être, la productivité et bien d'autres sujets encore.</li>
+          </ul>
+          </section>
+        
+        <section>
+          <h3>Rejoignez notre communauté</h3>
+          
+          <p>Nous vous invitons à vous joindre à notre communauté en vous abonnant à notre newsletter et en nous suivant sur les réseaux sociaux. C'est le meilleur moyen de rester informé des dernières mises à jour, articles et événements de Lghorba.</p>
+        </section>
+        
+        <section>
+          <h3>Contactez-nous</h3>
+          
+          <p>
+            Si vous avez des questions, des suggestions ou si vous souhaitez contribuer en tant qu'auteur invité, n'hésitez pas à nous contacter. Nous sommes toujours ravis d'entendre parler de nos lecteurs et de collaborer avec des passionnés comme vous !
+          </p>
+        </section>
+          <h1><center>Explorez, découvrez et partagez avec Lghorba.com !</center></h1>
+      </div>
+      <hr/>
+      
+      <footer>
+        <section style={{"direction": "rtl"}}>
+          <b>تابعونا على وسائل التواصل الاجتماعي:</b>
+          <ul>
+            <li>
+            :فيسبوك
+            </li>
+            <li>
+            تويتر:          </li>
+            <li>
+            إنستجرام:
+            </li>
+            <li>
+            يوتيوب:            </li>
+          </ul>
+        </section>
+        <section>
+          <b>Suivez-nous sur les réseaux sociaux :</b>
+          <ul>
+            <li>
+            Facebook:             </li>
+            <li>
+            Twitter:            </li>
+            <li>
+            Instagram:            </li>
+            <li>
+            YouTube:            </li>
+          </ul>
+        </section>
+      </footer>
+    </main>
+  );
+}
---
+++ app/privacy-policy/page.tsx
@@ -0,0 +1,86 @@
+import React from "react";
+
+const Privacy = () => {
+
+    return (
+        <article>
+            <section>
+                <h2>Privacy Policy for Lghorba</h2>
+                <p>
+                This Privacy Policy describes how Lghorba collects, uses, and discloses your personal information when you visit our website <a href="https://www.lghorba.com">lghorbadotcom</a>.
+                </p>
+            </section>
+
+        <section>
+            <h2>Information We Collect</h2>
+            <p>
+            We collect information you provide directly to us when you access or use our Site. This may include:
+            </p>
+            <ul>
+                <li>
+                Personal information such as your name, email address, and any other information you provide when contacting us or subscribing to our newsletter.
+            
+                </li>
+                <li>Usage data and technical information collected automatically when you interact with our Site, such as your IP address, browser type, and operating system.</li>
+            </ul>
+        </section>
+
+        <section>
+    <h2>Use of Information</h2>
+            <p>We may use the information we collect for various purposes, including to:</p>
+
+        <ul>
+            <li>Provide and maintain our Site.</li>
+            <li>Respond to your inquiries and communicate with you.</li>
+            <li>Send you newsletters, marketing communications, and other updates about our products and services, where you have </li>
+            <li>consented to receive such communications.</li>
+            <li>Monitor and analyze trends, usage, and activities in connection with our Site.</li>
+            <li>Detect, investigate, and prevent fraudulent or unauthorized activities.</li>
+        </ul>
+
+    </section>
+
+<section>
+    <h2>Sharing of Information</h2>
+       <p>We may share your information with third parties who provide services on our behalf or with whom we have partnered to offer a service. These third parties are obligated not to use or disclose your information except as necessary to perform services on our behalf or to comply with legal requirements.</p>
+       <br/>
+       <p>We may also disclose your information in the following circumstances:</p>
+       
+           <ul>
+               <li>In response to a subpoena or similar investigative demand, a court order, or a request for cooperation from law enforcement or other government agency.</li>
+               <li>To protect our rights, property, or safety or that of others.</li>
+               <li>In connection with any merger, sale of company assets, financing, or acquisition of all or a portion of our business by another company.</li>
+           </ul>
+</section>
+<section>
+    
+    <h2>Security</h2>
+    
+    <p>We take reasonable measures to help protect your information from unauthorized access, use, or disclosure. However, no method of transmission over the Internet, or method of electronic storage, is 100% secure. Therefore, while we strive to use commercially acceptable means to protect your information, we cannot guarantee its absolute security.</p>
+    
+</section>
+    <section>
+        <h2>Your Choices</h2>
+        
+        <p>
+            You may opt out of receiving promotional communications from us by following the instructions provided in those communications. If you opt out, we may still send you non-promotional communications, such as those about your account or our ongoing business relations.
+
+        </p>
+    </section>
+    
+    <section>
+        <h2>Changes to This Privacy Policy</h2>
+        
+        <p>We may update our Privacy Policy from time to time. We will notify you of any changes by posting the new Privacy Policy on this page.</p>
+    </section>
+<section>
+    
+    <h2>Contact Us</h2>
+    
+    <p>If you have any questions about this Privacy Policy, please contact us at lghorba.com.contact@gmail.com</p>
+</section>
+        </article>
+    )
+}
+
+export default Privacy;
